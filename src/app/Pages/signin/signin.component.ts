6b3b545118bcb0cc1cca4c171ce282b905314e78
sopehSalah <essopeh20012@gmail.com>
2024-05-01 11:49:33 +0300

    signin finish with services

--- src/app/Pages/signin/signin.component.ts
+++ src/app/Pages/signin/signin.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
+import { UsersService } from 'src/app/Service/users.service';
+import { LoginUser } from 'src/app/interfaces/loginUser';
+import { userToken } from 'src/app/interfaces/userToken';
 
 @Component({
   selector: 'app-signin',
@@ -7,15 +12,40 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent {
-  // myForm: FormGroup;
-
-  // constructor() {
-  //   this.myForm = new FormGroup({
-  //     username: new FormControl(''),
-  //   });
-  // }
+  constructor(public userService: UsersService, public router: Router) {}
   userLoginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
+  get getEmail() {
+    return this.userLoginForm.controls['email'];
+  }
+  get getPassword() {
+    return this.userLoginForm.controls['password'];
+  }
+  handleLogin(data: any) {
+    data.preventDefault();
+    if (this.userLoginForm.valid) {
+      const loginUser: LoginUser = {
+        email: this.userLoginForm.controls.email.value || '',
+        password: this.userLoginForm.controls.password.value || '',
+      };
+      this.userService.loginUser(loginUser).subscribe({
+        next: (response: any) => {
+          localStorage.setItem('_petsToken', response.token);
+          const decodedToken: userToken = jwtDecode(response.token);
+          this.userService.loggedinUser = decodedToken;
+          this.router.navigate(['/home']);
+        },
+        error: (error) => {
+          if (error.status === 401) {
+            console.log('Invalid email or password');
+            this.userLoginForm.controls.password.setErrors({
+              invalidLogin: true,
+            });
+          }
+        },
+      });
+    }
+  }
 }
---
+++ src/app/Service/users.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginUser } from '../interfaces/loginUser';
+import { userToken } from '../interfaces/userToken';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  baseUrl: string = 'https://localhost:44337/api/Account';
+
+  constructor(public http: HttpClient) {}
+
+  loginUser(user: LoginUser) {
+    return this.http.post(this.baseUrl + 'login', user);
+  }
+
+  loggedinUser: userToken = {
+    userID: '',
+    userEmail: '',
+    userFullName: '',
+    userRole: '',
+    userName: '',
+  };
+
+  retreiveTokenData() {
+    let token = localStorage.getItem('_petsToken');
+    if (token) {
+      let userToken: userToken = jwtDecode(token);
+      this.loggedinUser.userID = userToken.userID;
+      this.loggedinUser.userEmail = userToken.userEmail;
+      this.loggedinUser.userFullName = userToken.userFullName;
+      this.loggedinUser.userRole = userToken.userRole;
+      this.loggedinUser.userName = userToken.userName;
+    }
+  }
+
+  isUserLoggedIn(): boolean {
+    return this.loggedinUser.userID === '' ? false : true;
+  }
+
+  logoutUser() {
+    localStorage.removeItem('_petsToken');
+    this.loggedinUser = {
+      userID: '',
+      userEmail: '',
+      userFullName: '',
+      userRole: '',
+      userName: '',
+    };
+  }
+}
