b590be263e5bf8f582de970839c1e86366d2fa6f
Asmaa Hosny <99550925+asmaahosnyy@users.noreply.github.com>
2024-05-01 19:13:35 +0300

    create usersgrpups

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './Pages/signin/signin.component';
+import { UsersGroupsComponent } from './Pages/users-groups/users-groups.component';
 
-const routes: Routes = [{ path: '', component: SigninComponent }];
+const routes: Routes = [
+  { path: '', component: SigninComponent },
+  {path: 'users-groups',component: UsersGroupsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SigninComponent } from './Pages/signin/signin.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UsersGroupsComponent } from './Pages/users-groups/users-groups.component';
 
 @NgModule({
-  declarations: [AppComponent, SigninComponent],
+  declarations: [
+    AppComponent, 
+    SigninComponent, 
+    UsersGroupsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
