affcf46894b80d3eb366a78d5284bdc0f081b0a9
sopehSalah <essopeh20012@gmail.com>
2024-05-04 21:43:48 +0300

    pdf export handle

--- src/app/Pages/attendance-departure/attendance-departure.component.ts
+++ src/app/Pages/attendance-departure/attendance-departure.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import * as XLSX from 'xlsx';
-
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 @Component({
   selector: 'app-attendance-departure',
   templateUrl: './attendance-departure.component.html',
   styleUrls: ['./attendance-departure.component.css'],
 })
 export class AttendanceDepartureComponent {
-  constructor() { }
-
+  @ViewChild('content', { static: false }) content!: ElementRef;
   onFileChange(event: any) {
     const file = event.target.files[0];
     const reader: FileReader = new FileReader();
@@ -20,15 +20,15 @@ export class AttendanceDepartureComponent {
       const firstSheetName: string = workbook.SheetNames[0];
       const worksheet: XLSX.WorkSheet = workbook.Sheets[firstSheetName];
 
-      const jsonData: any[] = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      const jsonData: any[] = XLSX.utils.sheet_to_json(worksheet, {
+        header: 1,
+      });
 
-      // Extract column headers from the first row
       const headers = jsonData[0];
 
-      // Convert the rest of the rows into objects with keys from column headers
-      const formattedData = jsonData.slice(1).map(row => {
+      const formattedData = jsonData.slice(1).map((row) => {
         const formattedRow: any = {};
-        headers.forEach((header:any, index:any) => {
+        headers.forEach((header: any, index: any) => {
           formattedRow[header] = row[index];
         });
         return formattedRow;
@@ -38,5 +38,18 @@ export class AttendanceDepartureComponent {
     };
 
     reader.readAsArrayBuffer(file);
+  }
+
+  SavePDF(): void {
+    const table = this.content.nativeElement;
+    table
+      .querySelectorAll(`th:nth-child(${7}), td:nth-child(${7})`)
+      .forEach((cell: HTMLElement) => {
+        cell.style.display = 'none';
+      });
+
+    const doc = new jsPDF();
+    autoTable(doc, { html: table });
+    doc.save('table.pdf');
   }
 }
