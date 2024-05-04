997d6c14368c4ed8a57772bc3503b0cdaf767682
Hamdy Emad <hamdyemad274@gmail.com>
2024-05-04 18:12:11 +0200

    handled groups in User Role Form drop menu

--- src/app/Pages/user-register/user-register.component.ts
+++ src/app/Pages/user-register/user-register.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserRegisterService } from 'src/app/Service/user-register.service';
+import { UsersGroupsService } from 'src/app/Service/users-groups.service';
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent implements OnInit {
-  registrationForm: FormGroup = new FormGroup({});
+  registrationForm!: FormGroup;
+  groups: { id: string, name: string }[] = [];
 
-  constructor(private formBuilder: FormBuilder , private userRegisterService: UserRegisterService) { }
+  constructor(private formBuilder: FormBuilder , private userRegisterService: UserRegisterService , private userGroupsService: UsersGroupsService) { }
+
 
   ngOnInit(): void {
     this.registrationForm = this.formBuilder.group({
@@ -20,7 +23,24 @@ export class UserRegisterComponent implements OnInit {
       confirmPassword: ['', Validators.required], // Add confirmPassword field
       groupId: ['', Validators.required]
     }, { validators: this.passwordMatchValidator });
+      this.fetchGroups();
   }
+     fetchGroups(){
+       this.userGroupsService.getGroups().subscribe({
+         next: (response:any) => {
+           // Handle successful registration
+           this.groups = response.data.map((group: any) => {
+            return { id: group.id, name: group.name };
+          });
+                     console.log('Groups fetched successfully:', this.groups);
+          },
+         error: (error:any) => {
+           // Handle registration error
+           console.error('Error fetching groups:', error);
+         }
+       })
+     }
+
   passwordMatchValidator(formGroup: FormGroup) {
     const password = formGroup.get('password')?.value;
     const confirmPassword = formGroup.get('confirmPassword')?.value;
@@ -29,21 +49,21 @@ export class UserRegisterComponent implements OnInit {
 
 
 
-  registerUser(Event : any) {
-    Event.preventDefault();
+  registerUser(event: any) {
+    event.preventDefault();
     if (this.registrationForm.valid) {
       // Form is valid, perform form submission logic here
-      // console.log(this.registrationForm.value);
-      this.userRegisterService.registerUser(this.registrationForm.value).subscribe({
-        next: (response:any) => {
+      const formData = this.registrationForm.value;
+      this.userRegisterService.registerUser(formData).subscribe({
+        next: (response: any) => {
           // Handle successful registration
           console.log('Registration successful:', response);
         },
-        error: (error:any) => {
+        error: (error: any) => {
           // Handle registration error
           console.error('Registration error:', error);
         }
-      })
+      });
     } else {
       // Form is invalid, mark all fields as touched to display validation errors
       this.registrationForm.markAllAsTouched();
