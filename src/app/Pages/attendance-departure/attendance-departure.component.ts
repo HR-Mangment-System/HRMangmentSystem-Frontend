fb1c6109b0b5a6aeeb147906596d8d544255a563
Hamdy Emad <hamdyemad274@gmail.com>
2024-05-04 21:12:54 +0200

    added excel reader function

--- src/app/Pages/attendance-departure/attendance-departure.component.ts
+++ src/app/Pages/attendance-departure/attendance-departure.component.ts
@@ -1,8 +1,42 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-attendance-departure',
   templateUrl: './attendance-departure.component.html',
   styleUrls: ['./attendance-departure.component.css'],
 })
-export class AttendanceDepartureComponent {}
+export class AttendanceDepartureComponent {
+  constructor() { }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    const reader: FileReader = new FileReader();
+
+    reader.onload = (e: any) => {
+      const data: string = e.target.result;
+      const workbook: XLSX.WorkBook = XLSX.read(data, { type: 'binary' });
+
+      const firstSheetName: string = workbook.SheetNames[0];
+      const worksheet: XLSX.WorkSheet = workbook.Sheets[firstSheetName];
+
+      const jsonData: any[] = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+      // Extract column headers from the first row
+      const headers = jsonData[0];
+
+      // Convert the rest of the rows into objects with keys from column headers
+      const formattedData = jsonData.slice(1).map(row => {
+        const formattedRow: any = {};
+        headers.forEach((header:any, index:any) => {
+          formattedRow[header] = row[index];
+        });
+        return formattedRow;
+      });
+
+      console.log(formattedData);
+    };
+
+    reader.readAsArrayBuffer(file);
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { SigninComponent } from './Pages/signin/signin.component';
 import { UsersGroupsComponent } from './Pages/users-groups/users-groups.component';
 import { UserRegisterComponent } from './Pages/user-register/user-register.component';
 import { EmployeeComponent } from './Pages/employee/employee.component';
+import { AttendanceDepartureComponent } from './Pages/attendance-departure/attendance-departure.component';
 
 const routes: Routes = [
   { path: '', component: SigninComponent },
   {path: 'users-groups',component: UsersGroupsComponent},
   {path: 'user-register',component: UserRegisterComponent},
   {path: 'employee',component: EmployeeComponent},
+  {path: 'attendance-departure',component: AttendanceDepartureComponent}
 ];
 
 @NgModule({
