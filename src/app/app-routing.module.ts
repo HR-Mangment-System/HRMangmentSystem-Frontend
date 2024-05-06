ae10c7f18c9cd41d059fc1902644fdb1149e8d4c
sopehSalah <essopeh20012@gmail.com>
2024-05-07 01:00:59 +0300

    simple change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,14 +10,13 @@ import { AnnualHolidaysComponent } from './Pages/annual-holidays/annual-holidays
 
 const routes: Routes = [
   { path: '', component: SigninComponent },
-  {path: 'users-groups',component: UsersGroupsComponent},
-  {path: 'user-register',component: UserRegisterComponent},
-  {path: 'employee',component: EmployeeComponent},
-  {path: 'attendance-departure',component: AttendanceDepartureComponent},
-  {path: 'employee-salary',component: EmployeeSalaryComponent}
-  {path: 'attendance-departure',component: AttendanceDepartureComponent},
-  {path: 'annual-holidays',component: AnnualHolidaysComponent},
-
+  { path: 'users-groups', component: UsersGroupsComponent },
+  { path: 'user-register', component: UserRegisterComponent },
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'attendance-departure', component: AttendanceDepartureComponent },
+  { path: 'employee-salary', component: EmployeeSalaryComponent },
+  { path: 'attendance-departure', component: AttendanceDepartureComponent },
+  { path: 'annual-holidays', component: AnnualHolidaysComponent },
 ];
 
 @NgModule({
