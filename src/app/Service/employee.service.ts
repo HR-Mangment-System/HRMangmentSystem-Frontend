d4d5581a7a67126fc665956a43719fdbd6904cf1
sopehSalah <essopeh20012@gmail.com>
2024-05-14 01:00:51 +0300

    handel conflict

--- src/app/Service/employee.service.ts
+++ src/app/Service/employee.service.ts
@@ -1,29 +1,22 @@
-<<<<<<< HEAD
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-=======
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Employee } from '../Models/employee';
->>>>>>> 09be55c10a96880b4ecf0dd8649831373042e029
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-<<<<<<< HEAD
   baseUrl: string = 'https://localhost:44337/api/Employees';
   constructor(private http: HttpClient) {}
   getEmployees() {
     return this.http.get(this.baseUrl + '/GetAllEmployees');
-=======
+  }
   headers = new HttpHeaders({
     'Content-Type': 'application/json',
     // Add other headers as needed
   });
   options = { headers: this.headers };
   apiURL = 'http://localhost:5023/api/Employees/';
-  constructor(private http: HttpClient) {}
 
   addEmp(emp: Employee) {
     return this.http.post(this.apiURL + 'AddEmployee', emp, this.options);
@@ -37,6 +30,5 @@ export class EmployeeService {
     return this.http.delete(
       `${this.apiURL}DeleteEmployee?NationalId=${NationalId}`
     );
->>>>>>> 09be55c10a96880b4ecf0dd8649831373042e029
   }
 }
