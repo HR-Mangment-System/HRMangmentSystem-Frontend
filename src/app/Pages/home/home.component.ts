9afaa9dbd75dd7e72046f4feb2f2098e7798e453
Abdelwahab Mohamed <abdelwahab.mohammed98@gmail.com>
2024-05-29 03:51:32 +0300

    home page handled

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { UsersService } from 'src/app/Service/users.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  userData: any;
+  constructor(private UsersService: UsersService) {
+    
+  }
+  ngOnInit(): void {
+    this.UsersService.retreiveTokenData();
+    console.log(this.UsersService.loggedinUser);;
+    this.userData = this.UsersService.loggedinUser;
+  }
+
 
 }
